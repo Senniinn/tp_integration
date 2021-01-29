4feafbd27eab041396079b4c14a37606ee07ffd9
Genevée <damien.genevee@epsi.fr>
2021-01-29 09:18:36 +0100

    Initial commit

---
+++ src/test/java/epsi/i1/tp_integration/TpIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package epsi.i1.tp_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
